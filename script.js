42e06a073023437d90729f5d5b2a479e8cb8102b
bruninhosrs <brunorodrigues398@gmail.com>
2025-09-25 18:02:22 -0300

    Ajustando evento e função do link (change)

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const payBtn = document.querySelector("#payBtn");
 const cancelBtn = document.querySelector("#cancelBtn");
+const changeLink = document.querySelector("a");
 
 function handlePay() {
   console.log(`Você quer avançar para o pagamento!`);
 }
 
 function handleCancel() {
-    console.log(`Você quer cancelar o plano!`)
+  console.log(`Você quer cancelar o plano!`);
+}
+
+function handleChange(e) {
+  e.preventDefault();
+  console.log(`Você quer mudar o seu plano!`);
 }
 
 payBtn.addEventListener("click", handlePay);
 cancelBtn.addEventListener("click", handleCancel);
+changeLink.addEventListener("click", handleChange);
